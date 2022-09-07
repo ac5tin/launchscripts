221ecfa790a11d0b5583974d320da8a2c5aa2ffc
austin <austinchang4@gmail.com>
2022-09-07 18:00:35 +0800

    feat: postgres with persistence

---
+++ pg_persist.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+podman volume create pgdata
+
+podman run -it --rm \
+    -v pgdata:/home/postgres/data -e PGDATA=/home/postgres/data \
+    -e POSTGRES_USER=root -e POSTGRES_PASSWORD=password \
+    -e POSTGRES_DB=mydb  -p 5432:5432 postgres:14
+     
+
+
+# podman volume rm pgdata
