29f7b1bcd17e4082d664aeb46be0ffac92b500de
austin <austinchang4@gmail.com>
2022-11-30 10:52:30 +0800

    chore(kind): instructions for ingress nginx

--- kind.net.sh
+++ kind.net.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 kind create cluster --config ./kind.net.config.yml --name c0
+
+
+# Instructions for Custom Ingress NGINX for Kind
+echo 'Please follow the instructions for installing Ingress NGINX for Kind: https://kind.sigs.k8s.io/docs/user/ingress/  or you can run the following: '
+echo 'kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml'
