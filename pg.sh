5a3725557aeb506464d7e427fa69b10c22a748c2
austin <austinchang4@gmail.com>
2023-02-07 15:07:19 +0800

    chore(postgres): update to version 15

--- pg.sh
+++ pg.sh
@@ -2,4 +2,4 @@
 
 podman run --privileged -it --rm \
     -e POSTGRES_USER=root -e POSTGRES_PASSWORD=password \
-    -e POSTGRES_DB=mydb  -p 5432:5432 postgres:14
+    -e POSTGRES_DB=mydb  -p 5432:5432 postgres:15
--- pg_persist.sh
+++ pg_persist.sh
@@ -6,7 +6,7 @@ podman volume create pgdata
 podman run -it --rm \
     -v pgdata:/home/postgres/data -e PGDATA=/home/postgres/data \
     -e POSTGRES_USER=root -e POSTGRES_PASSWORD=password \
-    -e POSTGRES_DB=mydb  -p 5432:5432 postgres:14
+    -e POSTGRES_DB=mydb  -p 5432:5432 postgres:15
      
 
 
