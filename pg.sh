e358e00dd7b2003b61d7b3f727465d545eef7575
austin <austinchang4@gmail.com>
2024-04-19 23:59:26 +0800

    chore(postgres): use pg16

--- pg.sh
+++ pg.sh
@@ -2,4 +2,4 @@
 
 podman run --privileged -it --rm \
     -e POSTGRES_USER=root -e POSTGRES_PASSWORD=password \
-    -e POSTGRES_DB=mydb  -p 5432:5432 postgres:15
+    -e POSTGRES_DB=mydb  -p 5432:5432 postgres:16
