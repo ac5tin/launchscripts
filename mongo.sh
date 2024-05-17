0715762e62d93751abcf9d74d3663a27e6631d0b
austin <austinchang4@gmail.com>
2024-05-17 23:28:10 +0800

    refactor(mongo): mongo with replicaset

--- mongo.sh
+++ mongo.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+#podman run --privileged -it --rm \
+#     -p 27017:27017 mongo:latest
+
 podman run --privileged -it --rm \
-     -p 27017:27017 mongo:latest
+     -e MONGODB_REPLICA_SET_MODE=primary \
+     -e ALLOW_EMPTY_PASSWORD=yes \
+     -e MONGODB_ADVERTISED_HOSTNAME=localhost \
+     -p 27017:27017 bitnami/mongodb:latest
