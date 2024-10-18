a0b13971b9a2a200abcded2a95c788ec9478ff90
austin <austinchang4@gmail.com>
2024-10-18 23:42:49 +0800

    refactor(mongo): set memory

--- mongo.sh
+++ mongo.sh
@@ -3,8 +3,8 @@
 #podman run --privileged -it --rm \
 #     -p 27017:27017 mongo:latest
 
-podman run --privileged -it --rm \
-     -e MONGODB_REPLICA_SET_MODE=primary \
-     -e ALLOW_EMPTY_PASSWORD=yes \
-     -e MONGODB_ADVERTISED_HOSTNAME=localhost \
-     -p 27017:27017 bitnami/mongodb:latest
+podman run --privileged -it --rm -m 2g \
+  -e MONGODB_REPLICA_SET_MODE=primary \
+  -e ALLOW_EMPTY_PASSWORD=yes \
+  -e MONGODB_ADVERTISED_HOSTNAME=localhost \
+  -p 27017:27017 bitnami/mongodb:latest
