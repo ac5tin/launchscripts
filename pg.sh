5510a9e9e05c36cf9ffc53325777f4e59c2863a6
ser6pro <austinchang4@gmail.com>
2026-03-06 11:05:44 +0800

    chore(postgres): pg 17

--- pg.sh
+++ pg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 podman run --privileged -it --rm \
-    -e POSTGRES_USER=root -e POSTGRES_PASSWORD=password \
-    -e POSTGRES_DB=mydb  -p 5432:5432 postgres:16
+  -e POSTGRES_USER=root -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_DB=mydb -p 5432:5432 postgres:17
